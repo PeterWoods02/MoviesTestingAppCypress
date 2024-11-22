3230519d245584af54f952c6dad2a0904b3e359b
Diarmuid O Connor <doconnor@wit.ie>
2024-11-22 18:11:10 +0000

    Initial structure

---
+++ src/contexts/moviesContext.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+export const MoviesContext = React.createContext(null);
+
+const MoviesContextProvider = (props) => {
+  const [myReviews, setMyReviews] = useState( {} )  // NEW
+  const [favourites, setFavourites] = useState([]);
+
+  const addReview = (movie, review) => {   // NEW
+    setMyReviews( {...myReviews, [movie.id]: review } )
+  };
+
+  const addToFavourites = (movie) => {
+    let updatedFavourites = [...favourites];
+    if (!favourites.includes(movie.id)) {
+      updatedFavourites.push(movie.id);
+    }
+    setFavourites(updatedFavourites);
+  };
+
+  // We will use this function in a later section
+  const removeFromFavourites = (movie) => {
+    setFavourites(favourites.filter((mId) => mId !== movie.id));
+  };
+
+  return (
+    <MoviesContext.Provider
+      value={{
+        favourites,
+        addToFavourites,
+        removeFromFavourites,
+        addReview, 
+      }}
+    >
+      {props.children}
+    </MoviesContext.Provider>
+  );
+};
+
+export default MoviesContextProvider;
