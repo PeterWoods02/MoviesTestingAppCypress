9d4ba3b484fc00ee3d4739852195bfa65385de50
Peter Woods <woodspeter37@gmail.com>
2024-11-26 13:11:16 +0000

    E2E tests - Navigation test spec.

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
   e2e: {
-    "baseUrl": "http://localhost:3000/",   // NEW
+    "baseUrl": "http://localhost:3000/",
+    "viewportWidth": 1980,
+    "viewportHeight": 1080,
     setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
---
+++ cypress/e2e/navigation.cy.js
@@ -0,0 +1,36 @@
+let movies;
+let movieId; // Enola Holmes movie id
+
+describe("Navigation", () => {
+  before(() => {
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .its("body")
+      .then((response) => {
+        movies = response.results;
+      });
+  });
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  describe("From the home page to a movie's details", () => {
+    it("Should navigate to the movie details page and change the browser's URL", () => {
+      cy.get(".MuiCardActions-root").eq(0).contains("More Info").click();
+      cy.url().should("include", `/movies/${movies[0].id}`);
+    });
+  });
+  describe("Using the site header links", () => {
+    it("Should navigate via the button links", () => {
+      cy.get("button").contains("Favorites").click();
+      cy.url().should("include", `/favourites`);
+      cy.get("button").contains("Home").click();
+      cy.url().should("include", `/`);
+    });
+  });
+  describe("From the favourites page to a movie's details", () => {
+    // TODO
+  });
+});
